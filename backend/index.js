e56a16acb261a42f414d2db0fb59a7f8671c6ac8
Dapirz <daffa.irsandy@gmail.com>
2026-05-25 06:51:08 +0700

    feat: implementasi CRUD transaksi dan auto-update saldo

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const path = require('path');
 
 const transaksiRoutes = require('./routes/transaksiRoutes');
+const akunRoutes = require('./routes/akunRoutes');
+const kategoriRoutes = require('./routes/kategoriRoutes');
 
 const app = express();
 const PORT = 5000;
@@ -11,6 +13,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/transaksi', transaksiRoutes);
+app.use('/api/akun', akunRoutes);
+app.use('/api/kategori', kategoriRoutes);
 
 app.listen(PORT, () => {
     console.log(`=== SERVER HTTP BACKEND RUNNING ===`);
